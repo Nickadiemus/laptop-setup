09e9b3b148976ef57ea529255731e20526205ff6
Nick Sladic <nick.sladic@g.uky.edu>
2019-05-11 19:52:31 -0400

    initial repo push

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Welcome to my personal laptop setup
+# This is a simple script that will install basic utilities for your laptop (or Desktop)
+# Feel free to use/edit it at your disposal
+# Author -  Nick Sladic
+# Website - https://nicksladic.me
+# Github -  nickadiemus
